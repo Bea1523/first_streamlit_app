f01c690f082aeb643bfe9db4d6c11b2f231b2bd3
Bea1523 <148437432+Bea1523@users.noreply.github.com>
2023-10-20 11:21:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ streamlit.dataframe(my_data_rows)
 
 
 
-# new_fruits_selected = streamlit.multiselect("Picke some new fruits:", list(fruit_load_list.index),['Banana'])
-# new_fruits_to_show = fruit_load_list.loc[new_fruits_selected]
-# streamlit.dataframe(new_fruits_to_show)
+new_fruits_selected = streamlit.multiselect("Picke some new fruits:", list(fruit_load_list.index),['Banana'])
+new_fruits_to_show = fruit_load_list.loc[new_fruits_selected]
+streamlit.dataframe(new_fruits_to_show)
 
-streamlit.write('Thanks for adding', add_my_fruit)
-mycur.execute("insert into fruit_load_list values ('from streamlit')")
+# streamlit.write('Thanks for adding', add_my_fruit)
+# mycur.execute("insert into fruit_load_list values ('from streamlit')")
