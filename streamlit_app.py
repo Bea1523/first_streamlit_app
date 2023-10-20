3713b9676bdcd2d8304ba1e78c0005317443b909
Bea1523 <148437432+Bea1523@users.noreply.github.com>
2023-10-20 11:57:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ try:
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
- except URLErrir as e:
-     streamlit.error()
+ # except URLErrir as e:
+     # streamlit.error()
 
 
 
